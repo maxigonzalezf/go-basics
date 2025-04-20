5adcdc2b38776af1cd4a1da4017cc9177a76dba3
maxigonzalezf <maxigf95@gmail.com>
2025-04-20 19:36:37 -0300

    arrays

--- video_0/main.go
+++ video_0/main.go
@@ -34,10 +34,13 @@ func main() {
 	println("")
 	dataTypesGroup()
 	println("")
-	printInt(myConst) // utilizo funcion de otro archivo dentro del mismo package
+	printInt(myConst)                      // utilizo funcion de otro archivo dentro del mismo package
 	exported.PrintConstant(agrupatedConst) // funcion importada del package exported
 	println("")
 	exported.PrintConstants(myConst, myOtherConst, agrupatedConst, agrupatedConst2)
+	otherTypeOfAssignation()
+	myOtherTypes()
+	otherArrayTypes()
 }
 
 func dataTypesZeroValues() {
@@ -68,4 +71,41 @@ func dataTypesGroup() {
 		b    = true
 	)
 	fmt.Printf("%v, %v, %v", q, name, b)
+}
+
+func otherTypeOfAssignation() {
+	myNumber := 8
+	fmt.Println(myNumber)
+	myString := "my name is Maxi"
+	fmt.Println(myString)
+}
+
+func myOtherTypes() {
+	// array []int = {1, 2, 3, 4} // INVALID OPERATION
+	arr := []int{1, 2, 3, 4, 5}
+
+	// for loop convencional
+	for i := 0; i < 10; i++ {
+		// ...
+		break
+	}
+
+	// for mas indicado para recorrer arrays
+	for index, value := range arr {
+		fmt.Printf("%v) %v", index, value)
+		fmt.Println()
+	}
+
+	// len() devuelve el largo del arreglo
+	arrLen := len(arr)
+	fmt.Println(arrLen)
+}
+
+func otherArrayTypes() {
+	var sl []int // nil
+	sl = []int{1, 2, 3, 4, 5}
+	fmt.Printf("Slice: %v", sl)
+	// la funcion append agrega el 6 y devuelve el nuevo slice con el valor agregado
+	sl = append(sl, 6)
+	fmt.Printf("Slice: %v", sl)
 }
